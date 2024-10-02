ebe64b4c7b3c427e22d8b6aab7672eafaf4a07be
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2024-10-02 18:58:33 +0300

    group routes

--- app/(content)/layout.js
+++ app/(content)/layout.js
@@ -1,10 +1,9 @@
-import './globals.css';
-import MainHeader from "@/components/main-header";
-
+import MainHeader from '@/components/main-header';
+import '../globals.css';
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
