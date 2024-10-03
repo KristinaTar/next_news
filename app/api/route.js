57d205729ea745ada567c243171d536463a43ce7
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2024-10-03 15:23:36 +0300

    optional features

---
+++ app/api/route.js
@@ -0,0 +1,11 @@
+//this functions just for example how to use route
+
+export function GET({request}){
+  return new Response("Test")
+
+}
+
+export function POST({request}){
+  return new Response("Post")
+
+}
